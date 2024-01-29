824e4ace8fa8cae9de466704076043f5369ddd60
Reenat <reenat056@yandex.ru>
2024-01-29 22:02:35 +0300

    add functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,13 +7,13 @@ fun main(args: Array<String>) {
 data class Chat(
     var id: Int = 0, //id самого чата
     val idInterlocutor: Int, //id собеседника
-) {
-    lateinit var message: Message
-}
+    var countMessage: Int = 0,
+    var messages: MutableList<Message> //пустой список для хранения сообщений
+)
 
 data class Message(
     var id: Int = 0, //id сообщения
-    val idChat: Int, //id чата, к которому принадлежит сообщение
+    val idChat: Int = 0, //id чата, к которому принадлежит сообщение
     val text: String, //тело сообщения
     var read: Boolean = false //прочитано сообщение или нет
 )
@@ -21,25 +21,36 @@ data class Message(
 class ChatService {
     //создаем пустой список для хранения чатов
     private val chats: MutableList<Chat> = mutableListOf()
+    //счетчик количества чатов
+    var countChat: Int = 0
 
-    //создаем пустой список для хранения сообщений в чатах
-    private val messages: MutableList<Message> = mutableListOf()
+    //создаем пустой список для хранения всех сообщений в чатах
+    private val messagesAll: MutableList<Message> = mutableListOf()
 
     //счетчик количества сообщений в чате
     var countMessage: Int = 0
 
     //вводим функцию для добавления чата с собеседником по его idInterlocutor
-    fun addChat(chat: Chat): Int {
-        chats += chat.copy() //добавляем копию (чтобы к нему не остался доступ извне по той же ссылке) чата в список всех чатов пользователя
-        chat.id = chats.size //присваиваем id чата значение равное размеру списка чатов
-        return chat.id
+    fun addChat(idInterlocutor: Int, message: Message): MutableList<Chat> {
+        //добавляем сообщение в список всех сообщений
+        messagesAll += message.copy(id = ++countMessage)
+        //увеличиваем счетчик чатов на 1
+        countChat += 1
+        chats += Chat(
+            id = countChat,
+            idInterlocutor = idInterlocutor,
+            countMessage = countMessage + 1,
+            //берем из списка всех сообщений нужное нам
+            messages = messagesAll.filter { it.idChat == countChat }.toMutableList()
+        )
+        return chats
     }
 
     //вводим функцию добавления сообщения в чат
     fun addMessage(idChat: Int, message: Message): Int {
         for ((index, item) in chats.withIndex()) {
             if (item.id == idChat) {
-                messages += message.copy(id = ++countMessage) //добавляем копию сообщения в чат с присвоением id
+                item.messages += message.copy(id = ++countMessage) //добавляем копию сообщения в чат с присвоением id
                 ++countMessage //обновляем счетчик сообщений
                 return message.id
             }
@@ -47,10 +58,21 @@ class ChatService {
         throw ChatIdNotFoundException("Чата с таким id не существует")
     }
 
+
     //введем функцию, которая будет возвращать чаты с непрочитанными сообщениями используя anonymous function
-    fun getUnreadChatsCount(chats: MutableList<Chat>): List<Chat> {
-        //т.е. если в свойстве чата message свойство read == false, сообщение считается непрочитанным
-        return chats.filter(fun (chat: Chat) = !chat.message.read)
+    fun getUnreadChatsCount(): MutableList<Chat> {
+        //создаем пустой список для хранения чатов с непрочитанными сообщениями
+        val mes: MutableList<Chat> = mutableListOf()
+
+        for (item in chats) {
+            for ((index, value) in item.messages.withIndex()) {
+                if (!value.read) {
+                    mes += item.copy()
+                }
+            }
+        }
+        //если сообщений нет или они все прочитаны возвращаем пустой список
+        return mes
     }
 
     //введем функцию для получения списка чатов
@@ -58,17 +80,56 @@ class ChatService {
         return chats.joinToString()
     }
 
-    //вводим функцию для получения последних сообщений из чатов в ввиде строк
+    //вводим функцию для получения последних сообщений из чата в ввиде строк
     fun lastMessageInAllChats(chats: MutableList<Chat>, idChat: Int): Message {
         //перебираем все чаты циклом
         for ((index, item) in chats.withIndex()) {
             //если id чата совпадает с нужным
             if (item.id == idChat) {
                 //возвращаем последнее сообщение из списка всех чатов с фильтрацией по idChat, к которому принадлежит сообщение
-                return messages.last { it.idChat == idChat }
+                return item.messages.last { it.idChat == idChat }
             }
         }
+        return Message(text = "Сообщений еще нет или они были удалены")
     }
+
+
+    //вводим функцию для вывода всех сообщений с определенным собеседником
+    fun getMessageInChat(idInterlocutor: Int, countMessage: Int) : List<Message> {
+        for ((index, item) in chats.withIndex()) {
+            if (item.idInterlocutor == idInterlocutor) {
+                //возвращает элементы из списка messages, idChat которые равны заданному id
+                return item.messages.filter(fun (message: Message) = message.idChat == item.id)
+            }
+        }
+        throw InterlocutorNotFoundException("Собеседника с таким id не существует")
+    }
+
+    //вводим функцию для удаления сообщения по id
+    fun deleteMessage(idChat: Int, idMessage: Int): Boolean {
+        for ((index, item) in chats.withIndex()) {
+            if (item.id == idChat) {
+                for ((count, value) in item.messages) {
+                    item.messages.removeAt(count)
+                    return true
+                }
+            }
+        }
+        throw MessageIdNotFoundException("Сообщение с таким id не существует")
+    }
+
+    //вводим функцию для удаления chat вместе с сообщениями, если они есть
+    fun deleteChat(idChat: Int): Boolean {
+        for ((index, item) in chats.withIndex()) {
+            if (item.id == idChat) {
+                chats.removeAt(index)
+            }
+        }
+        throw ChatIdNotFoundException("Чата с таким id не существует")
+    }
+
 }
 
 class ChatIdNotFoundException(message: String) : RuntimeException(message)
+class InterlocutorNotFoundException(message: String) : RuntimeException(message)
+class MessageIdNotFoundException(message: String) : RuntimeException(message)
